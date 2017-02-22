0815f952c1eaca02aedd64f8be035b026cf9f675
vagrant <yusra.ahmed05@gmail.com>
2017-02-22 04:34:04 +0000

    continue with exercise-extraction error

--- practice3.sh
+++ practice3.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #sample data extraction
 
 DATARECORD="last=Clifford,first=Johnny Boy,state=CA"
@@ -21,3 +22,20 @@ echo ${STRING[@]/#to be/eat now} #replace occurrence of substring if at beginnin
 echo ${STRING[@]/%be/eat} #replace occurrence of substring if at end
 
 echo ${STRING[@]/%be/be on $(date +%Y-%m-%d)} # to be or not to be on 2017-02-21
+
+
+BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
+
+    # write your code here
+    ISAY=$BUFFETT
+    change1=${ISAY[@]/snow/foot}
+    change2=${change1[@]// snow/}
+    change3=${change2[@]/finding/getting}
+    loc=`expr index "$change3" 'w'`
+    ISAY=${change3::$loc+2}
+
+# Test code - do not modify
+echo "Warren Buffett said:"
+echo $BUFFETT
+echo "and I say:"
+echo "$ISAY"
