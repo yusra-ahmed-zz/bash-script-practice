d7d4e42f0fbf66414e2c1714226fd4aa0c0aa060
vagrant <yusra.ahmed05@gmail.com>
2017-02-22 03:12:28 +0000

    practice continued

---
+++ practice2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NAMES=( John Eric Jessica )
+
+# write your code here
+NUMBERS=( 1 2 3 )
+STRINGS=( hello world )
+#get total number of elements
+NumberOfNames=${#NAMES[@]}
+#reference by index
+second_name=${NAMES[1]}
+
+
+COST_PINEAPPLE=50
+COST_BANANA=4
+COST_WATERMELON=23
+COST_BASKET=1
+TOTAL=$(($COST_PINEAPPLE + (($COST_BANANA * 2)) + (($COST_WATERMELON * 3)) + $COST_BASKET))
+echo "Total Cost is $TOTAL"
